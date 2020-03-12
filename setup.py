db6170d8c37f9b9cf874d5a995e0c67f36214939
Sergey Tereschenko <serg.partizan@gmail.com>
2020-03-12 10:44:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="novaposhta-api-client",
-    version="0.2.1",
+    version="0.2.2",
     description="Python client for Nova Poshta company's API",
     # long_description=open("README.md").read(),
     # long_description_content_type="text/markdown",
